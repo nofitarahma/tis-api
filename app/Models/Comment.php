937b282d7096dad44d42786532989432d3fd26e5
nofitarahma <novikarahma542@gmail.com>
2026-03-18 13:46:51 +0700

    modul 4

---
+++ app/Models/Comment.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Comment extends Model
+{
+    protected $fillable = [
+        'review',
+        'post_id'
+    ];
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,26 @@
 <?php
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-
-Route::get(
-    '/filterByParam/{author}/{year}',
-    [BookController::class, 'filterBooksByParam']
-)->middleware('validate.year');
-
-Route::get('/filterByQuery', [BookController::class, 'filterBooksByQuery']);
-
-Route::prefix('books')->group(function () {
-
-    Route::get('/', [BookController::class, 'getAll'])->name('books.all');
-
-    Route::get('/{id}', [BookController::class, 'getOne']);
-
-    Route::post('/', [BookController::class, 'create']);
-
-    Route::put('/{id}', [BookController::class, 'update']);
-
-    Route::delete('/{id}', [BookController::class, 'delete']);
-
+Route::prefix('posts')->group(function () {
+    Route::post('/', [PostController::class, 'createPost']);
+    Route::get('/{id}', [
+        PostController::class,
+        'getPostById'
+    ]);
+    Route::put('/{id}/tag/{tagId}', [
+        PostController::class,
+        'addTag'
+    ]);
 });
+Route::prefix('comments')->group(function () {
+    Route::post('/', [
+        CommentController::class,
+        'createComment'
+    ]);
+});
+Route::prefix('tags')->group(function () {
+    Route::post('/', [TagController::class, 'createTag']);
+});
+
